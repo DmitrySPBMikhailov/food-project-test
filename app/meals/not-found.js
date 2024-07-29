9b334b756f37b6187bd4f5b239737b1fb9bfaba6
DmitrySPBMikhailov <dmitry004@mail.ru>
2024-07-29 12:45:54 +0300

    food test project

---
+++ app/meals/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundMeal = () => {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>We cound not find the requested page</p>
+    </main>
+  );
+};
+
+export default NotFoundMeal;
